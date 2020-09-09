10f638ab5d0b57adedef69ede96119fb914bdca5
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-09 06:00:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -155,7 +155,8 @@ end
 def team_colors(name_of_team)
   game_hash.find do |h_a, df_h_a|
     if game_hash[h_a][:team_name] == name_of_team do 
-       
+       game_hash[h_a][:colors]
+    end
   end
 end
 binding.pry
