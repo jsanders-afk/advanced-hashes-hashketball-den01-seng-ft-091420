d2f7cd2dc7bd2648daefa5287f9284ffd3312649
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-09 07:02:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -201,9 +201,15 @@ end
 
 
 def big_shoe_rebounds
-  
+  game_hash.each do |h_a, df_h_a|
+    game_hash[h_a][:players].each do |p_s|
+      if p_s[:shoe] == name
+         return p_s
+      end
+    end
+  end
 end
-
+binding.pry
 
 
 
