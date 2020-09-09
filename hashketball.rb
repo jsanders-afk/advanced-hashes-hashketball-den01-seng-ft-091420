70ef847fa91ef4ee879277a8e7d844830a61b957
Jack Sanders <guitarbreakerjs@gmail.com>
2020-09-09 07:27:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -207,7 +207,7 @@ def big_shoe_rebounds
       if p_s[:shoe] > mv
         puts mv = p_s[:shoe]
       end
-      if p_s[:shoe] = mv
+      if p_s[:shoe] == mv
          p_s[:shoe]
       end
     end
